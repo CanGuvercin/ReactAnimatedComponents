1c248fe11bcbad0fafc3a93505ba6149dd1cd1b7
Can Guvercin <mustafacanguvercin@gmail.com>
2025-08-23 23:21:37 +0300

    contacts

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+export default function Contacts() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>This is the Contacts page.</p>
+      <p>       </p>
+        <p>More content coming soon!</p>
+        
+    </div>
+  );
+}
